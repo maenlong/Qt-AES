64f6c9db257aee97ce5993ea7a0b1dec2bcbc896
MEL <15801230835@163.com>
2024-07-01 14:08:20 +0800

    [code review]调整函数入参，调试加密失败问题

--- example/main.cpp
+++ example/main.cpp
@@ -10,19 +10,23 @@ int main(int argc, char *argv[])
     /****************************************************************************************************/
     {
         QAESEncryption encryption(QAESEncryption::AES_128, QAESEncryption::ECB , QAESEncryption::ISO);
-        QByteArray input = QString("HELLO AES").toLocal8Bit();
-        QByteArray key = QString("your-string-key").toLocal8Bit();
+        QString inputStr("HELLO AES");
+        QString key("your-string-key");
+        QString iv("your-IV-vector");
 
-        QByteArray encodeText = encryption.encode(input, key);
-        QByteArray decodeText = encryption.decode(encodeText, key);
+        QByteArray hashKey = QCryptographicHash::hash(key.toLocal8Bit(), QCryptographicHash::Sha256);
+        QByteArray hashIV = QCryptographicHash::hash(iv.toLocal8Bit(), QCryptographicHash::Md5);
+
+        QByteArray encodeText = encryption.encode(inputStr.toLocal8Bit(), hashKey, hashIV);
+        QByteArray decodeText = encryption.decode(encodeText, hashKey, hashIV);
 
         QString decodedString = QString(QAESEncryption::RemovePadding(decodeText,QAESEncryption::ISO));
 
-        qDebug() << " \n input = " << input
+        qDebug() << " \n input = " << inputStr
                      << " \n encodeText = " << encodeText
-                     << " \n QString = " << QString(encodeText)
-                     << " \n QString(utf8) = " << QString::fromUtf8(encodeText)
-                     << " \n QString(local8bit) = " << QString::fromLocal8Bit(encodeText)
+//                     << " \n QString = " << QString(encodeText)
+//                     << " \n QString(utf8) = " << QString::fromUtf8(encodeText)
+//                     << " \n QString(local8bit) = " << QString::fromLocal8Bit(encodeText)
                      << " \n decodeText = " << decodeText
                      << " \n output = " << decodedString
                      << " \n " ;
